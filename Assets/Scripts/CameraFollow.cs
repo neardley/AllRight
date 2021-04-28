109efc64577675670031c20fe86b202cf96c3285
Max Tribble <mtribble@trinity.edu>
2021-04-28 10:10:55 -0500

    rolled back

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
 
+[RequireComponent(typeof(PhotonView))]
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
@@ -9,17 +13,48 @@ public class CameraFollow : MonoBehaviour
     public float smoothSpeed = 40;
     public float rotationSmoothSpeed = 40;
 
+    public PhotonView photonView;
+
+    private void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+        StartCoroutine(FindTarget());
+    }
+
+    IEnumerator FindTarget()
+    {
+        GameObject[] playerObjs = GameObject.FindGameObjectsWithTag("Player");
+
+        //if not all players are found retry after 0.1 seconds
+        while (playerObjs.Length != PhotonNetwork.PlayerList.Length)
+        {
+            yield return new WaitForSeconds(0.1f);
+            playerObjs = GameObject.FindGameObjectsWithTag("Player");
+        }
+
+        foreach (GameObject playerObj in playerObjs)
+        {
+            if (PhotonView.Get(playerObj).IsMine)
+            {
+                target = playerObj.transform;
+            }
+        }
+    }
+
     void FixedUpdate()
     {
-        transform.position = Vector3.Lerp(
+        if (target != null)
+        {
+            transform.position = Vector3.Lerp(
             transform.position,
             target.TransformPoint(0f, height, -distance),
             smoothSpeed * Time.deltaTime
-        );
-        transform.rotation = Quaternion.Slerp(
-            transform.rotation,
-            Quaternion.LookRotation(target.position - transform.position + (Vector3.up * offsetTargetY), Vector3.up),
-            rotationSmoothSpeed * Time.deltaTime
-        );
+            );
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                Quaternion.LookRotation(target.position - transform.position + (Vector3.up * offsetTargetY), Vector3.up),
+                rotationSmoothSpeed * Time.deltaTime
+            );
+        }
     }
 }
