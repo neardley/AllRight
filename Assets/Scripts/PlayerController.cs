689f8b29058a7f264ec0aea79d48e103b6cd7e9b
Max Tribble <mtribble@trinity.edu>
2021-04-28 12:52:23 -0500

    fixed player tracking

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -42,7 +42,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody>();
     }
