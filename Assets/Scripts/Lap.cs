1571355d85f4bd8a66e12ed11f9524e5b76392e5
Conor Greene <cgreene1@trinity.edu>
2021-04-27 23:34:06 -0500

    Added checkpoint functionality, other testing for Conor's scene, will implement checkpoints in level tomorrow

---
+++ Assets/Scripts/Lap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lap : MonoBehaviour
+{
+    public int lap = 0;
+    public bool hasWon;
+    public GameObject player, finish;
+    public Checkpoint chck;
+    
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        chck = GetComponent<Checkpoint>();
+        yield return new WaitForEndOfFrame();
+        player = GameObject.FindGameObjectWithTag("Player");
+        finish = GameObject.FindGameObjectWithTag("FnshLine");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasWon == true) {
+            Debug.Log("You have won!");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if ((other.gameObject.tag == "Player") && (chck.checkList[1].Item2 == true)) {
+            lap++;
+            Debug.Log(lap);
+        }
+        if (lap == 3) {
+            hasWon = true;
+        }
+    }
+}
