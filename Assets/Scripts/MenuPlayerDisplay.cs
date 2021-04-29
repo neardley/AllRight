27c4f0aefcde00056d37e1ca0a4fae24d5da5701
Max Tribble <mtribble@trinity.edu>
2021-04-29 10:53:17 -0500

    player setup framework

---
+++ Assets/Scripts/MenuPlayerDisplay.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+//This handels updating the networkPlayerPanels in the main menu
+//todo set carDisplay to color and remove color display texxt
+public class MenuPlayerDisplay : MonoBehaviour
+{
+    TextMeshProUGUI playerNameText, isReadyText;
+    Image carDisplay;
+    int targetPlayer = -1;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerNameText = transform.Find("PlayerNameText").gameObject.GetComponent<TextMeshProUGUI>();
+        isReadyText = transform.Find("IsReadyText").gameObject.GetComponent<TextMeshProUGUI>();
+        carDisplay = transform.Find("CarDisplay").gameObject.GetComponent<Image>();
+
+        //hide panel on start
+        transform.localScale = Vector3.zero;
+    }
+
+    public void SetTarget(int newTargetIndex)
+    {
+        Debug.Log("Setting target to: " + targetPlayer.ToString());
+        targetPlayer = newTargetIndex;
+    }
+
+    public void UpdateUI()
+    {
+        
+        if (targetPlayer != -1|| targetPlayer > PhotonNetwork.CurrentRoom.PlayerCount)
+        {
+            transform.localScale = Vector3.one;
+            Player player = PhotonNetwork.CurrentRoom.Players[targetPlayer];
+            playerNameText.text = player.NickName;
+            carDisplay.color = ToColor((string)player.CustomProperties["Color"]);
+            if ((string)player.CustomProperties["Ready"] == "true")
+            {
+                isReadyText.text = "Ready";
+            }
+            else isReadyText.text = "Not Ready";
+        }
+        else transform.localScale = Vector3.zero;
+    }
+
+    Color ToColor(string colorName)
+    {
+        return (Color) typeof(Color).GetProperty(colorName.ToLowerInvariant()).GetValue(null, null);
+    }
+}
---
+++ Assets/Scripts/PlayerSetupPanel.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+public class PlayerSetupPanel : MonoBehaviourPunCallbacks
+{
+    Transform readyButton, colorButtonPanel;
+    TextMeshProUGUI playerNameText;
+    Image carDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        readyButton = transform.Find("ReadyButton");
+        colorButtonPanel = transform.Find("ColorButtonPanel");
+        playerNameText = transform.Find("PlayerNameText").gameObject.GetComponent<TextMeshProUGUI>();
+        carDisplay = transform.Find("CarDisplay").gameObject.GetComponent<Image>();
+
+        playerNameText.text = "Name: " + PhotonNetwork.NickName;
+        OnChangeColor("Green");
+        setReady(false);
+    }
+    public void OnToggleReady()
+    {
+        TextMeshProUGUI readyButtonText = readyButton.gameObject.GetComponentInChildren<TextMeshProUGUI>();
+
+        if ((string)PhotonNetwork.LocalPlayer.CustomProperties["Ready"] == "true")
+        {
+            setReady(false);
+            colorButtonPanel.gameObject.SetActive(true);
+            readyButtonText.text = "Ready";
+        }
+        else
+        {
+            setReady(true);
+            colorButtonPanel.gameObject.SetActive(false);
+            readyButtonText.text = "UnReady";
+        }
+    }
+
+    void setReady(bool isReady)
+    {
+        Hashtable hash = new Hashtable();
+        if (isReady) hash.Add("Ready", "true");
+        else hash.Add("Ready", "false");
+        PhotonNetwork.SetPlayerCustomProperties(hash);
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+    {
+        carDisplay.color = ToColor((string)PhotonNetwork.LocalPlayer.CustomProperties["Color"]);
+    }
+
+    public void OnChangeColor(string newColor)
+    {
+        Hashtable hash = new Hashtable();
+        hash.Add("Color", newColor);
+        PhotonNetwork.SetPlayerCustomProperties(hash);
+    }
+
+    Color ToColor(string colorName)
+    {
+        return (Color)typeof(Color).GetProperty(colorName.ToLowerInvariant()).GetValue(null, null);
+    }
+}
